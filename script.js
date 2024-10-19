abdd3d679559b9051ecc8450929c2b1c0b2144a7
jaivanth <jubijaivanth@gmail.com>
2024-10-19 13:32:02 +0530

    removed

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const weather_body = document.querySelector('.weather-body');
 
 
 async function checkWeather(city){
-    const api_key = "e577f02595dfc6c5fadf589665566d15";
+    const api_key = "api-key";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
     const weather_data = await fetch(`${url}`).then(response => response.json());
